5ef91c169f2ad7aa8546aa0b314a142c0b5245e3
Dmitriy <dmi3y.kochetkov@gmail.com>
2020-12-02 18:26:46 +0300

    censored commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from imaginator.entry import create_video
 
 if __name__ == '__main__':
-    create_video(name='hello_grn', frame_rate=24.0, text_line='привет пидор!')
+    create_video(name='hello', frame_rate=24.0, text_line='hello world!')
